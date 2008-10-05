6acb456b40f7923c154673f9cea5975864fe58cf
Marc-André Lanciault <marcan@impresscms.org>
2008-10-05 13:25:48 +0000

    Incluing SimplePie
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imfeeding/trunk@5394 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/common.php
+++ include/common.php
@@ -13,14 +13,19 @@ if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
 if(!defined("IMFEEDING_DIRNAME"))		define("IMFEEDING_DIRNAME", $modversion['dirname'] = basename(dirname(dirname(__FILE__))));
 if(!defined("IMFEEDING_URL"))			define("IMFEEDING_URL", ICMS_URL.'/modules/'.IMFEEDING_DIRNAME.'/');
-if(!defined("IMFEEDING_ROOT_PATH"))	define("IMFEEDING_ROOT_PATH", ICMS_ROOT_PATH.'/modules/'.IMFEEDING_DIRNAME.'/');
+if(!defined("IMFEEDING_ROOT_PATH"))		define("IMFEEDING_ROOT_PATH", ICMS_ROOT_PATH.'/modules/'.IMFEEDING_DIRNAME.'/');
 if(!defined("IMFEEDING_IMAGES_URL"))	define("IMFEEDING_IMAGES_URL", IMFEEDING_URL.'images/');
-if(!defined("IMFEEDING_ADMIN_URL"))	define("IMFEEDING_ADMIN_URL", IMFEEDING_URL.'admin/');
+if(!defined("IMFEEDING_ADMIN_URL"))		define("IMFEEDING_ADMIN_URL", IMFEEDING_URL.'admin/');
 
 // Include the common language file of the module
 icms_loadLanguageFile('imfeeding', 'common');
 
 include_once(IMFEEDING_ROOT_PATH . "include/functions.php");
+
+// Loading IcmsSimpleRSS
+$simplerss_filename = ICMS_ROOT_PATH . '/class/icmssimplerss.php';
+if (!file_exists($simplerss_filename)) $simplerss_filename = IMFEEDING_ROOT_PATH . 'class/icmssimplerss.php';
+include_once($simplerss_filename);
 
 // Creating the module object to make it available throughout the module
 $imfeedingModule = icms_getModuleInfo(IMFEEDING_DIRNAME);

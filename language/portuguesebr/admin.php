aab83a30da8096a3ff60008f310c5caeebc3475b
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-11-04 13:23:54 +0000

    portuguesebr - imfeeding - Started translation...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imfeeding/trunk@6396 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/portuguesebr/admin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* English language constants used in admin section of the module
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+// general
+define("_AM_IMFEEDING_FEEDS", "Feeds");
+define("_AM_IMFEEDING_FIRST_USE", "This is the first time you access this module. Please update the module in order to dynamically create the database scheme.");
+
+// Feed
+define("_AM_IMFEEDING_FEEDS_DSC", "All feeds in the module");
+define("_AM_IMFEEDING_FEED_CREATE", "Add a feed");
+define("_AM_IMFEEDING_FEED", "Tag");
+define("_AM_IMFEEDING_FEED_CREATE_INFO", "Fill-out the following form to create a new feed.");
+define("_AM_IMFEEDING_FEED_EDIT", "Edit this feed");
+define("_AM_IMFEEDING_FEED_EDIT_INFO", "Fill-out the following form in order to edit this feed.");
+define("_AM_IMFEEDING_FEED_MODIFIED", "The feed was successfully modified.");
+define("_AM_IMFEEDING_FEED_CREATED", "The feed has been successfully created.");
+define("_AM_IMFEEDING_FEED_VIEW", "Tag info");
+define("_AM_IMFEEDING_FEED_VIEW_DSC", "Here is the info about this feed.");
+
+?>

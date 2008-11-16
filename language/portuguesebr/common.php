89277d5a478c8a5b552e4a605438f71906182439
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-11-16 17:15:51 +0000

    Portuguesebr - ImFeeding - Updated with last version and Finished Translation... started re-tests with last version 1.1.1 branch...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imfeeding/trunk@6990 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/common.php
+++ language/portuguesebr/common.php
@@ -7,7 +7,7 @@
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
-* @translation        GibaPhp - http://br.impresscms.org 
+* @translation  GibaPhp - http://br.impresscms.org 
 */
 
 if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
